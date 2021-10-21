ddb33edb2f0cb092ddff58f24f980a6734eda141
Jejoon Ryu <rjejoon@gmail.com>
2021-10-21 04:45:51 -0600

    Getter and Setters for all class fields. Input validations for title and reason. Add one TODO.

--- code/app/src/main/java/com/cmput301f21t26/habittracker/objects/Habit.java
+++ code/app/src/main/java/com/cmput301f21t26/habittracker/objects/Habit.java
@@ -1,31 +1,76 @@
 package com.cmput301f21t26.habittracker.objects;
 
-import android.os.Build;
-
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Habit {
 
     private String title;
     private String reason;
-    private LocalDate startDate;
+    private Date startDate;
+    private boolean isDoneForToday;
 
-    public Habit(String title, String reason, LocalDate startDate) {
+    /* TODO implement after implementing HabitEvent and HabitPlan
+    private List<HabitEvent> habitEvents;
+    private List<HabitPlan> plan;
+    */
+
+    public Habit(String title, String reason, Date startDate) {
+        if (title.length() > 20) {
+            throw new IllegalArgumentException("Habit title must be up to 20 characters");
+        }
         this.title = title;
+        if (reason.length() > 30) {
+            throw new IllegalArgumentException("Habit reason must be up to 30 characters");
+        }
         this.reason = reason;
         this.startDate = startDate;
+        this.isDoneForToday = false;
+    }
+
+    public Habit(String title, Date startDate) {
+        this(title, "", startDate);
     }
 
     public Habit(String title) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            this(title, "", LocalDate.now());
-        } else {
-            Date startDate = new Date();
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        this(title, "", Calendar.getInstance().getTime());
+    }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        if (title.length() > 20) {
+            throw new IllegalArgumentException("Habit title must be up to 20 characters");
         }
+        this.title = title;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        if (reason.length() > 30) {
+            throw new IllegalArgumentException("Habit reason must be up to 30 characters");
+        }
+        this.reason = reason;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public boolean isDoneForToday() {
+        return isDoneForToday;
+    }
+
+    public void setDoneForToday(boolean doneForToday) {
+        isDoneForToday = doneForToday;
     }
 }
