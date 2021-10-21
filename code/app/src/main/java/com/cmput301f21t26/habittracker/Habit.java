5fdfa49ecbe76fb43d1ac2ed079c864db4c3112b
Jejoon Ryu <rjejoon@gmail.com>
2021-10-20 22:11:10 -0600

    Start Habit class constructor

--- code/app/src/main/java/com/cmput301f21t26/habittracker/Habit.java
+++ code/app/src/main/java/com/cmput301f21t26/habittracker/Habit.java
@@ -1,4 +1,31 @@
 package com.cmput301f21t26.habittracker;
 
+import android.os.Build;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Habit {
+
+    private String title;
+    private String reason;
+    private LocalDate startDate;
+
+    public Habit(String title, String reason, LocalDate startDate) {
+        this.title = title;
+        this.reason = reason;
+        this.startDate = startDate;
+    }
+
+    public Habit(String title) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            this(title, "", LocalDate.now());
+        } else {
+            Date startDate = new Date();
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+
+        }
+    }
 }
