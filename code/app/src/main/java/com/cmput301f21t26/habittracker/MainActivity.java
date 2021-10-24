0b8edd15cc0ad39db8b851ac3d67f74deaadfb6b
combo199 <rmo1@ualberta.ca>
2021-10-24 12:23:30 -0600

    Fixed issue with bottom nav not navigating to fragments

--- code/app/src/main/java/com/cmput301f21t26/habittracker/MainActivity.java
+++ code/app/src/main/java/com/cmput301f21t26/habittracker/MainActivity.java
@@ -1,12 +1,20 @@
 package com.cmput301f21t26.habittracker;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 
+import com.cmput301f21t26.habittracker.ui.home.HomeFragment;
+import com.cmput301f21t26.habittracker.ui.profile.ProfileFragment;
+import com.cmput301f21t26.habittracker.ui.timeline.TimelineFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
@@ -14,6 +22,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.cmput301f21t26.habittracker.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,19 +42,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
 
         addHabitButton = findViewById(R.id.addHabitButton);
-        addHabitButtonStatic = findViewById(R.id.addHabitButton);
+        addHabitButtonStatic = findViewById(R.id.addHabitButton);           // used in hiding bottom nav bar...needs to be static
         extendBottomNav = findViewById(R.id.extendBottomNav);
 
         navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_timeline, R.id.navigation_profile)
                 .build();
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
         assert navHostFragment != null;
         navController = navHostFragment.getNavController();
+
+        // Clicking on icons navigate to the selected fragments
+        navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                int id = item.getItemId();
+                if (id == R.id.navigation_home) {
+                    selectedFragment = new HomeFragment();
+                }
+                if (id == R.id.navigation_timeline) {
+                    selectedFragment = new TimelineFragment();
+                }
+                if (id == R.id.navigation_profile) {
+                    selectedFragment = new ProfileFragment();
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_container, selectedFragment).commit();
+                return true;
+            }
+        });
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
@@ -57,12 +88,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+
+    /**
+     * Hides the bottom navigation bar
+     */
     public static void hideBottomNav(){
         navView.setVisibility(View.GONE);
         addHabitButtonStatic.setVisibility(View.INVISIBLE);
         extendBottomNav.setVisibility(View.INVISIBLE);
     }
 
+    /**
+     * Shows the bottom navigation bar
+     */
     public static void showBottomNav(){
         navView.setVisibility(View.VISIBLE);
         addHabitButtonStatic.setVisibility(View.VISIBLE);
