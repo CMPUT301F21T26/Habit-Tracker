3217d9c405f807f7ccde3fda6e744b8f73ad5de2
combo199 <rmo1@ualberta.ca>
2021-10-23 23:51:50 -0600

    Fixed navController bug; Add habit button now navigates to add habit fragment!

--- code/app/src/main/java/com/cmput301f21t26/habittracker/MainActivity.java
+++ code/app/src/main/java/com/cmput301f21t26/habittracker/MainActivity.java
@@ -1,12 +1,15 @@
 package com.cmput301f21t26.habittracker;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
@@ -14,26 +17,57 @@ import com.cmput301f21t26.habittracker.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static BottomNavigationView navView;
+    private static Button addHabitButtonStatic;
+    private static View extendBottomNav;
     private ActivityMainBinding binding;
+    private NavController navController = null;
+    private Button addHabitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        addHabitButton = findViewById(R.id.addHabitButton);
+        addHabitButtonStatic = findViewById(R.id.addHabitButton);
+        extendBottomNav = findViewById(R.id.extendBottomNav);
+
+        navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-        /*
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
+        assert navHostFragment != null;
+        navController = navHostFragment.getNavController();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-        */
+
+        addHabitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.addHabitFragment);
+            }
+        });
+    }
+
+    public static void hideBottomNav(){
+        navView.setVisibility(View.GONE);
+        addHabitButtonStatic.setVisibility(View.INVISIBLE);
+        extendBottomNav.setVisibility(View.INVISIBLE);
+    }
+
+    public static void showBottomNav(){
+        navView.setVisibility(View.VISIBLE);
+        addHabitButtonStatic.setVisibility(View.VISIBLE);
+        extendBottomNav.setVisibility(View.VISIBLE);
+
     }
 
 }
--- code/app/src/main/java/com/cmput301f21t26/habittracker/fragments/AddHabitFragment.java
+++ code/app/src/main/java/com/cmput301f21t26/habittracker/fragments/AddHabitFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.cmput301f21t26.habittracker.MainActivity;
 import com.cmput301f21t26.habittracker.R;
 
 public class AddHabitFragment extends Fragment {
@@ -26,5 +27,17 @@ public class AddHabitFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_habit, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        MainActivity.hideBottomNav();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        MainActivity.showBottomNav();
     }
 }
