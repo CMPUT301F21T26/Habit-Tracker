0e27b42aa827d8a195cd2141e335afcf74258140
Jejoon Ryu <rjejoon@gmail.com>
2021-10-23 18:23:31 -0600

    Implement constructors, getters, and setters of the HabitEvent class Add javadoc for appropriate methods.

--- code/app/src/main/java/com/cmput301f21t26/habittracker/objects/HabitEvent.java
+++ code/app/src/main/java/com/cmput301f21t26/habittracker/objects/HabitEvent.java
@@ -1,4 +1,105 @@
 package com.cmput301f21t26.habittracker.objects;
 
+import android.location.Location;
+import android.net.Uri;
+
+import androidx.annotation.Nullable;
+
+import java.util.Calendar;
+import java.util.Date;
+
 public class HabitEvent {
+
+    private String comment;
+    private Location loc;
+    private Uri photo_uri;
+    private Date hEventDate;
+
+    /**
+     * Default constructor of HabitEvent
+     *
+     * @param comment optional string comment up to 20 characters
+     * @param loc optional location information
+     * @param photo_uri optional photo uri
+     * @param hEventDate date at which the event occurred
+     * @throws IllegalArgumentException if habit event comment is too long
+     */
+    public HabitEvent(String comment, @Nullable Location loc, @Nullable Uri photo_uri, Date hEventDate) {
+        if (comment.length() > 20) {
+            throw new IllegalArgumentException("Habit event comment must be up to 20 characters");
+        }
+        this.comment = comment;
+        this.loc = loc;
+        this.photo_uri = photo_uri;
+        this.hEventDate = hEventDate;
+    }
+
+    /**
+     * A constructor for the case where the location and the photograph are not given.
+     * In this case, both are set to null.
+     *
+     * @param comment optional string comment up to 20 characters
+     * @param hEventDate date at which the event occurred
+     * @throws IllegalArgumentException if habit event comment is too long
+     */
+    public HabitEvent(String comment, Date hEventDate) {
+        this(comment, null, null, hEventDate);
+    }
+
+    /**
+     * A constructor for the case where the location and habit event date are not given.
+     * In this case, location and photo uri are set to null and the date is set
+     * to the time where the constructor is invoked.
+     *
+     * @param comment optional string comment up to 20 characters
+     * @throws IllegalArgumentException if habit event comment is too long
+     */
+    public HabitEvent(String comment) {
+        this(comment, null, null, Calendar.getInstance().getTime());
+    }
+
+    /**
+     * A constructor for creating an empty habit event.
+     * The default comment is an empty string, the default location and photo uri are null, and
+     * the default habit event date is the time where the constructor is invoked.
+     *
+     * @throws IllegalArgumentException if habit event comment is too long
+     */
+    public HabitEvent() {
+        this("", null, null, Calendar.getInstance().getTime());
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    /**
+     * Sets habit event comment
+     *
+     * @param comment optional string comment up to 20 characters
+     * @throws IllegalArgumentException if habit event comment is too long
+     */
+    public void setComment(String comment) {
+        if (comment.length() > 20) {
+            throw new IllegalArgumentException("Habit event comment must be up to 20 characters");
+        }
+        this.comment = comment;
+    }
+
+    public Location getLocation() {
+        return loc;
+    }
+
+    public void setLocation(Location loc) {
+        this.loc = loc;
+    }
+
+    public Date getHabitEventDate() {
+        return hEventDate;
+    }
+
+    public void setHabitEventDate(Date hEventDate) {
+        this.hEventDate = hEventDate;
+    }
+
 }
