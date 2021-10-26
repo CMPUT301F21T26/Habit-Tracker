dfa737e987642a02b663861138ececd39098e6c4
UAlbertaSu <tzuhang@ualberta.ca>
2021-10-25 21:08:33 -0600

    adding the saved habit into database

--- code/app/src/main/java/com/cmput301f21t26/habittracker/fragments/AddHabitFragment.java
+++ code/app/src/main/java/com/cmput301f21t26/habittracker/fragments/AddHabitFragment.java
@@ -7,19 +7,38 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.cmput301f21t26.habittracker.MainActivity;
 import com.cmput301f21t26.habittracker.R;
+import com.cmput301f21t26.habittracker.objects.Habit;
 
-public class AddHabitFragment extends Fragment {
+import java.util.Date;
+
+public class AddHabitFragment extends Fragment{
+
+    Button confirmButton;
+    FirebaseFirestore mStore;
+
+
+
+
+    /**
+     * required empty public constructor
+     */
 
     public AddHabitFragment() {
         // Required empty public constructor
+
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mStore = FirebaseFirestore.getInstance();
+
+
     }
 
     @Override
@@ -30,9 +49,47 @@ public class AddHabitFragment extends Fragment {
     }
 
     @Override
+    public void onCick(View view){
+
+        //make sure user clicks on confirm button before storing data into firebase
+
+        if (view.getId() == R.id.confirmHabitButton){
+
+            //if the user did not give startDate
+            if (reason.isEmpty()){
+                Habit habit = new Habit("", "");
+                storeData(habit);
+            }
+            // if the user did not give us a reason or start date
+            if (reason.isEmpty() & date.isEmpty()){
+                Habit habit = new Habit("");
+                storeData(habit);
+
+            }
+            //if the user entered everything
+            else{
+                Habit habit = new Habit("","", "");
+                storeData(habit);
+            }
+
+        }
+
+    }
+
+    public void storeData(Habit habit){
+
+        mStore.collection("users").document(habit);
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
         MainActivity.hideBottomNav(getActivity().findViewById(R.id.addHabitButton), getActivity().findViewById(R.id.extendBottomNav));
+
+
+
+
+
     }
 
     @Override
