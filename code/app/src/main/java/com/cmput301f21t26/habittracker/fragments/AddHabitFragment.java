1b4634bc9df3820c8f9d018a674e3f505f3f7b42
HarmanRan <91702630+HarmanRan@users.noreply.github.com>
2021-10-25 22:13:26 -0600

    deleted unused drawable

--- code/app/src/main/java/com/cmput301f21t26/habittracker/fragments/AddHabitFragment.java
+++ code/app/src/main/java/com/cmput301f21t26/habittracker/fragments/AddHabitFragment.java
@@ -24,6 +24,7 @@ public class AddHabitFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
     }
 
     @Override
