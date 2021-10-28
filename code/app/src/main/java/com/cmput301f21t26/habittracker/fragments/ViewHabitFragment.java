8c3799e3f24b00f70c3d6d5ca8852abe29f6bf21
combo199 <rmo1@ualberta.ca>
2021-10-27 23:25:36 -0600

    Hide bottom nav and menu items when view ViewHabitFragment

--- code/app/src/main/java/com/cmput301f21t26/habittracker/fragments/ViewHabitFragment.java
+++ code/app/src/main/java/com/cmput301f21t26/habittracker/fragments/ViewHabitFragment.java
@@ -2,12 +2,16 @@ package com.cmput301f21t26.habittracker.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.cmput301f21t26.habittracker.MainActivity;
 import com.cmput301f21t26.habittracker.R;
 
 public class ViewHabitFragment extends Fragment {
@@ -26,5 +30,29 @@ public class ViewHabitFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_view_habit, container, false);
+    }
+
+    /**
+     * Hides menu items in add habit fragment
+     *
+     * @param menu
+     * @param inflater
+     */
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        MainActivity.hideMenuItems(menu);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        MainActivity.hideBottomNav(getActivity().findViewById(R.id.addHabitButton), getActivity().findViewById(R.id.extendBottomNav));
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        MainActivity.showBottomNav(getActivity().findViewById(R.id.addHabitButton), getActivity().findViewById(R.id.extendBottomNav));
     }
 }
