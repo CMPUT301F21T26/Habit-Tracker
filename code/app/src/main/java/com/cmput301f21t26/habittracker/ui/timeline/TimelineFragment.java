c7313c35bca506e08624959bf81a0ee0e8b39df8
combo199 <rmo1@ualberta.ca>
2021-10-29 18:18:43 -0600

    Timeline fragment kinda done; need way to get habit events into list

--- code/app/src/main/java/com/cmput301f21t26/habittracker/ui/timeline/TimelineFragment.java
+++ code/app/src/main/java/com/cmput301f21t26/habittracker/ui/timeline/TimelineFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,13 +12,34 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.cmput301f21t26.habittracker.R;
 import com.cmput301f21t26.habittracker.databinding.FragmentTimelineBinding;
+import com.cmput301f21t26.habittracker.objects.HabitEvent;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class TimelineFragment extends Fragment {
 
     private TimelineViewModel timelineViewModel;
     private FragmentTimelineBinding binding;
+    private ListView timelineListView;
+    private TimelineListAdapter timelineListAdapter;
+    private ArrayList<HabitEvent> habitEventsList;
+    private NavController navController;
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore mStore;
+    private String username;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,19 +49,25 @@ public class TimelineFragment extends Fragment {
         binding = FragmentTimelineBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        timelineViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-
-        if (getActivity().getActionBar() != null) {
-
-        }
-
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+        username = mAuth.getCurrentUser().getDisplayName();
+        mStore = FirebaseFirestore.getInstance();
+
+        navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_activity_main);
+
+        // TODO get all habit events from all habits and store them in habitEventsList, sort by date
+
+        // config ListView
+        timelineListView = view.findViewById(R.id.timelineListView);
+        timelineListAdapter = new TimelineListAdapter(getActivity(), habitEventsList);
+        timelineListView.setAdapter(timelineListAdapter);
     }
 
     @Override
