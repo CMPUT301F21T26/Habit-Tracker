547502e6640b8391c52b0c6164d9bbdeede4fcfd
Jejoon Ryu <rjejoon@gmail.com>
2021-10-31 18:00:11 -0600

    Remove unused imports

--- code/app/src/main/java/com/cmput301f21t26/habittracker/fragments/SignupFragment.java
+++ code/app/src/main/java/com/cmput301f21t26/habittracker/fragments/SignupFragment.java
@@ -20,10 +20,6 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.cmput301f21t26.habittracker.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -33,7 +29,6 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
 import java.util.Calendar;
 import java.util.HashMap;
