5767f528bdd912a15eda8bedd0cda1de7cfd1ee8
combo199 <rmo1@ualberta.ca>
2021-11-03 17:54:41 -0600

    ViewHabitFragmentTest now works!!!!

--- code/app/src/androidTest/java/com/cmput301f21t26/habittracker/ViewHabitEventFragmentTest.java
+++ code/app/src/androidTest/java/com/cmput301f21t26/habittracker/ViewHabitEventFragmentTest.java
@@ -1,11 +1,16 @@
 package com.cmput301f21t26.habittracker;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.Matchers.hasToString;
 
 import android.util.Log;
 
@@ -31,16 +36,37 @@ public class ViewHabitEventFragmentTest {
         }
 
         Thread.sleep(1000);
-        onView(withText("navigation_timeline")).perform(click());
+        onView(withId(R.id.navigation_timeline)).perform(click());
+        onView(withId(R.id.navigation_timeline)).check(matches(isDisplayed()));
+
     }
 
+    /**
+     * Tests if the views are shown
+     */
     @Test
-    public void testViewHabitEventDetails() {
-        onView(withText("Test")).perform(click());
-        onView(withId(R.id.viewHabitEventFragment)).check(matches(isDisplayed()));
+    public static void testViewHabitEventDetailsShown() throws InterruptedException {
+        Thread.sleep(1000);
+        onData(anything()).inAdapterView(withId(R.id.timelineListView)).atPosition(0).perform(click());
         onView(withId(R.id.habitEventLocationTV)).check(matches(isDisplayed()));
         onView(withId(R.id.viewHabitEventTitleTV)).check(matches(isDisplayed()));
         onView(withId(R.id.habitEventDateFormatTV)).check(matches(isDisplayed()));
         onView(withId(R.id.habitEventCommentET)).check(matches(isDisplayed()));
+        onView(withId(R.id.habitEventImage)).check(matches(isDisplayed()));
+        onView(withId(R.id.confirmHabitEventButton)).check(matches(isDisplayed()));
+        onView(withId(R.id.editHabitEventButton)).check(matches(isDisplayed()));
+    }
+
+    /**
+     * Tests if clicking the checkmark button
+     * brings the user back
+     */
+    @Test
+    public void testFinish() throws InterruptedException {
+        Thread.sleep(1000);
+        onData(anything()).inAdapterView(withId(R.id.timelineListView)).atPosition(0).perform(click());
+        onView(withId(R.id.confirmHabitEventButton)).perform(click());
+        // Should bring user back to the timeline fragment
+        onView(withId(R.id.navigation_timeline)).check(matches(isDisplayed()));
     }
 }
