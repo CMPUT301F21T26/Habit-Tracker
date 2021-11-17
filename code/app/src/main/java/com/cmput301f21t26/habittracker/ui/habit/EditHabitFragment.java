7134b2e391e5b78d7f3f896cca705e83e2791293
combo199 <rmo1@ualberta.ca>
2021-11-16 19:52:05 -0700

    Changed Habit Fragments to use the global navigation and safe args instead of bundles; made edit button invisible if not current user

--- code/app/src/main/java/com/cmput301f21t26/habittracker/ui/habit/EditHabitFragment.java
+++ code/app/src/main/java/com/cmput301f21t26/habittracker/ui/habit/EditHabitFragment.java
@@ -123,10 +123,8 @@ public class EditHabitFragment extends Fragment {
 
         chooseDateButton.setOnClickListener(v -> datePicker.show(requireActivity().getSupportFragmentManager(), "start date"));
 
-        assert getArguments() != null;
-        String habitId = getArguments().getString("habitId");
         // grabs habit and updates screen with this information
-        habit = UserController.getHabit(habitId);
+        habit = EditHabitFragmentArgs.fromBundle(getArguments()).getHabit();
         updateScreen();
 
         // make the confirm button and delete buttons clickable
