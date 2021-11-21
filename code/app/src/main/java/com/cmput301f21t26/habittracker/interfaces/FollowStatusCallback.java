76005d8c4670ee12db8e98e0a69e8482775d32f9
Jejoon Ryu <rjejoon@gmail.com>
2021-11-21 06:06:39 -0700

    Implement following, unfollowing, accepting and denying follow requests

---
+++ code/app/src/main/java/com/cmput301f21t26/habittracker/interfaces/FollowStatusCallback.java
@@ -0,0 +1,7 @@
+package com.cmput301f21t26.habittracker.interfaces;
+
+import com.cmput301f21t26.habittracker.objects.FollowStatus;
+
+public interface FollowStatusCallback {
+    void onCallback(FollowStatus status);
+}
