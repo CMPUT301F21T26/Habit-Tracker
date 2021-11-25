0304ad826643102407b2ccff8447deba1d1f2fd0
UAlbertaSu <77360262+UAlbertaSu@users.noreply.github.com>
2021-11-24 19:46:54 -0700

    get default location but not finished.

--- code/app/src/main/java/com/cmput301f21t26/habittracker/ui/MapFragment.java
+++ code/app/src/main/java/com/cmput301f21t26/habittracker/ui/MapFragment.java
@@ -1,6 +1,10 @@
 package com.cmput301f21t26.habittracker.ui;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -8,57 +12,92 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.cmput301f21t26.habittracker.R;
 import com.cmput301f21t26.habittracker.databinding.FragmentMapBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private FragmentMapBinding binding;
     private GoogleMap mMap;
+    private FusedLocationProviderClient client;
+    private Location location;
+    private double lat;
+    private double lng;
+    private LocationCallback locationCallback;
 
     /**
      * Required empty public constructor
      */
-    public MapFragment() { }
+    public MapFragment() {
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        binding = FragmentMapBinding.inflate(inflater, container, false);
 
+        binding = FragmentMapBinding.inflate(inflater, container, false);
+        client = LocationServices.getFusedLocationProviderClient(this.getActivity());
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
 
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    lat = location.getLatitude();
+                    lng = location.getLongitude();
+                }
+            }
+        };
+
         return binding.getRoot();
     }
+
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this.getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Task<Location> task = client.getLastLocation();
+            if (task.isSuccessful() && task.getResult() != null) {
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(53.52332731030619, -113.5261898552411);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("uofa"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                // Add a marker to current location and move the camera
+                LatLng current = new LatLng(lat, lng);
+                mMap.addMarker(new MarkerOptions().position(current).title("Current Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(current));
 
+            }
+        }
     }
+
 
     /**
      * Hides menu items
@@ -76,11 +115,23 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public void onResume() {
         super.onResume();
         MainActivity.hideBottomNav(getActivity().findViewById(R.id.addHabitButton), getActivity().findViewById(R.id.extendBottomNav));
+        if (requestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    private void startLocationUpdates() {
+        client.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
     }
 
     @Override
     public void onStop() {
         super.onStop();
         MainActivity.showBottomNav(getActivity().findViewById(R.id.addHabitButton), getActivity().findViewById(R.id.extendBottomNav));
+        stopLocationUpdates();
+    }
+
+    private void stopLocationUpdates() {
+        client.removeLocationUpdates(locationCallback);
     }
 }
